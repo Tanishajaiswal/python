ddc36afb520d707addedd3be1ee09dcd636c16af
Tanisha <86921810+Tanishajaiswal@users.noreply.github.com>
2021-07-12 09:51:44 +0530

    tuple method.py
    this file shows different methods in tuple

---
+++ tuple method.py
@@ -0,0 +1,8 @@
+#correct syntax to print the number of items in a tuple
+stationary=("book","pen","scale","eraser")
+print(len(stationary))
+
+#correct syntax for indexing in a tuple
+stationary=("book","pen","scale","eraser")
+print(stationary[0])
+
